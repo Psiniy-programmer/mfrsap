5ffdde6fc8913db895c239332c5138046eb420b9
Psiniy-programmer <psiniy-programmer@yandex.ru>
2020-09-23 09:27:57 +0300

    deleted linkData

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,7 +7,6 @@ import auditoryList from './auditoryList.js';
 import raspData from './raspData.js';
 import scrolls from './scrolls.js';
 import sideBarToggler from './sideBarToggler.js';
-import linkData from './linkData.js';
 
 export default combineReducers({
     groupsList,
@@ -17,6 +16,5 @@ export default combineReducers({
     raspData,
     filterItems,
     scrolls,
-    sideBarToggler,
-    linkData
+    sideBarToggler
 });
