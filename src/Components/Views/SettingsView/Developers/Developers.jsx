0760539df98faa357d169e5f30f1f127295a448a
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-03-05 02:09:46 +0300

    margins fixes

--- src/Components/Views/SettingsView/Developers/Developers.jsx
+++ src/Components/Views/SettingsView/Developers/Developers.jsx
@@ -8,7 +8,7 @@ class Developers extends Component {
                 <h2 className={'text-bold--header textColor'}>Информация о разработчиках</h2>
                 <div className={'developers__items'}>
                     <p className={'text-regular--medium textColor'}>Frontend: Дмитрий Овденко.</p>
-                    <p className={'text-regular--medium textColor'}>Backend: Артем Назаров, Казбек Ахмедов.</p>
+                    <p className={'text-regular--medium textColor'}>Backend: Артем Назаров. </p>
                     <p className={'text-regular--medium textColor'}>Design: Анастасия Подворная.</p>
                 </div>
             </>
