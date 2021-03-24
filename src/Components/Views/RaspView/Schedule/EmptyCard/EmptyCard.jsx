db6af8d997412f73639b04c1a0689df52055efb7
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-03-24 16:58:55 +0300

    emptyCard hyphen

--- src/Components/Views/RaspView/Schedule/EmptyCard/EmptyCard.jsx
+++ src/Components/Views/RaspView/Schedule/EmptyCard/EmptyCard.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import consts from "../../../../../helpers/consts";
 import "./style.css";
 
 class EmptyCard extends Component {
-  render() {
-    const {soon, children} = this.props;
+  getHyphen() {
+    const isMobile = this.props.windowWidth < consts.DESKTOP_MIN_WIDTH;
 
-    return (
-      <div className={`emptyCard ${soon ? 'soon emptyCard__red' : 'scheduleColor'}`}>
+    if (!isMobile) {
+      return <>
+        <p className="hyphen hyphen_start text-bold--large">—</p>
+        <p className="hyphen hyphen_end text-bold--large">—</p>
+      </>;
+    }
+  }
+
+  render() {
+    const { soon, children } = this.props;
+
+    return <>
+      <div
+        className={`emptyCard text-bold--large ${
+          soon ? "soon emptyCard__red" : "scheduleColor"
+        }`}
+      >
         {children}
-        <p className="emptyCard__text text-bold--large">Занятия нет</p>
+        <p className="emptyCard__text">Занятия нет</p>
       </div>
-    );
+      {this.getHyphen()}
+    </>;
   }
 }
 
-export default EmptyCard;
+const mapStateToProps = (state) => {
+  return {
+    windowWidth: state.windowSizes.width,
+  };
+};
+
+export default connect(mapStateToProps)(EmptyCard);
