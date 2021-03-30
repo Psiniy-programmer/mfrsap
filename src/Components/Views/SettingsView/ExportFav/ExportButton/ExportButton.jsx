3b9a9f004bd92c34ab4c80fba540a62b2dcf8b7c
psiniy-programmer <psiniy-programmer@yandex.ru>
2021-03-30 04:12:01 +0300

    height fixes and PopUp

--- src/Components/Views/SettingsView/ExportFav/ExportButton/ExportButton.jsx
+++ src/Components/Views/SettingsView/ExportFav/ExportButton/ExportButton.jsx
@@ -18,13 +18,24 @@ class ExportButton extends Component {
 
   setHandleClick() {
     const code = "DKLAD7#ARJL^JLR9393#";
-    localStorage.setItem(key, code);
-    this.setState({ generated: code });
+    // localStorage.setItem(key, code);
+    // this.setState({ generated: code });
+    let test = {"groups":["К5-61Б"],"teachers":["Пересунько Е.А.","Малашин А.А."],"auditoryies":["365"]}
+    let response = fetch('https://mf.bmstu.ru/rasp/api/favorites', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(test)
+    })
+    .then(result => console.log('res', result))
+    .catch(er => console.log('er', er));
   }
 
   render() {
     const { generated } = this.state;
     const { isMobile, func } = this.props;
+    console.log(JSON.stringify(this.props.storage));
 
     if (generated === null) {
       return (
@@ -56,6 +67,7 @@ class ExportButton extends Component {
 const mapStateToProps = (state) => {
   return {
     isMobile: state.windowSizes.isMobile,
+    storage: state.favoriteStorage
   };
 };
 
